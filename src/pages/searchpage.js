a4b96cfb1c2b7edbc81e9221cf6595389cca96c6
Jay Goss <jayspgoss@gmail.com>
2019-02-04 02:02:46 -0500

    adding pages

--- src/pages/searchpage.js
+++ src/pages/searchpage.js
@@ -1,10 +1,11 @@
 import React, { Component, fragment } from "react";
 // import logo from './logo.svg';
-import Navbar from "./components/Navbar";
-import BookSearch from "./components/BookSearch/search";
-import Results from "./components/Results/results";
-import Jumbotron from "./components/Jumbotron/jumbotron";
-import "./App.css";
+import Navbar from "../components/Navbar";
+import BookSearch from "../components/BookSearch/search";
+import Results from "../components/Results/results";
+import Jumbotron from "../components/Jumbotron/jumbotron";
+import API from "../utils/API";
+// import "./App.css";
 
 class Searchpage extends Component {
   render() {
